6e5ee08d7833806d30a17d1c6aa3f6f79492a8cd
Cuong Nguyen <ancuongnguyen07@gmail.com>
2023-01-11 14:18:46 +0200

    [overflow_attacks] function pointer, and string format vulnerabilities

--- Ex1_Buffer_Overflow/overflow_attacks.c
+++ Ex1_Buffer_Overflow/overflow_attacks.c
@@ -5,26 +5,24 @@
 #include <unistd.h>
 #include <sys/types.h>
 
-#define FIXED_SIZE 256
+#define BUFFERSIZE 16
 #define OVERSIZE 5
 
-void unlimited_BO(){
+void unlimited_BO(char *mal_input){
     // Initialize a memory are containing fixed 256 bytes
-    char buffer[FIXED_SIZE];
+    char buffer[BUFFERSIZE];
 
-    // Send to the buffer up to 512 bytes
-    for (unsigned int i = 0; i <  512; i++){
-        buffer[i] = 'A';
-    }
+    // un-checked input string
+    strcpy(buffer, mal_input);
 }
 
-void off_by_one_BO(){
-    char buffer[FIXED_SIZE];
+void off_by_one_BO(char *mal_input){
+    char buffer[BUFFERSIZE];
 
     // using 'less than or equal to' instead of 'less than'
     // by mistake
-    for (unsigned int i = 0; i <= FIXED_SIZE; i++){
-        buffer[i] = 'A';
+    for (unsigned int i = 0; i <= BUFFERSIZE; i++){
+        buffer[i] = mal_input[i];
     }
 }
 
@@ -38,14 +36,40 @@ void heap_overflow(){
     strcpy(buffer, "AAAAAAAAAAAAAAA");
     printf("Buffer before Heap Overflow: \t%s\n", buffer);
 
-    memset(malicious_input, 'P', (unsigned int)(diff*OVERSIZE));
+    memset(malicious_input, 'P', (unsigned int)(diff));
     printf("Buffer after Heap Overflow: \t%s\n", buffer);
 }
 
-int main(){
-    // off_by_one_BO();
-    // unlimited_BO();
+void good_function(const char* foobar){
+    printf("Say: %s\n", foobar);
+}
+
+void funcptr_overflow(char* valid_input, char* mal_input){
+    char buffer[BUFFERSIZE];
+    void (*funcptr)(const char* foobar);
+
+    funcptr = good_function;
+    strcpy(buffer, mal_input);
+    (*funcptr)(valid_input);
+}
+
+void printf_vul(char* mal_input){
+    // char buffer[BUFFERSIZE];
+
+    printf(mal_input);
+}
+
+int main(int argc, char** argv){
+    char buffer[BUFFERSIZE];
+
+    // printf("%s", argv[1]);
+    // off_by_one_BO(argv[1]);
+    // unlimited_BO(argv[1]);
     // heap_overflow();
+
+    
+    strncpy(buffer, argv[1], 15);
+    printf_vul(buffer);
 
     return 0;
 }
